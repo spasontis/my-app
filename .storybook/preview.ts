9954e102cdf9bd6e9fc986385e48cc8bb5265a65
Dmitrii <smxxtdzmail@gmail.com>
2025-10-12 11:40:08 +0700

    chore: setup storybook

---
+++ .storybook/preview.ts
@@ -0,0 +1,39 @@
+import type { Preview } from "@storybook/nextjs-vite";
+import {
+  Title,
+  Subtitle,
+  Description,
+  Primary,
+  Controls,
+  Stories,
+} from "@storybook/addon-docs/blocks";
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+
+    a11y: {
+      // 'todo' - show a11y violations in the test UI only
+      // 'error' - fail CI on a11y violations
+      // 'off' - skip a11y checks entirely
+      test: "todo",
+    },
+    docs: {
+      page: () => (
+        <>
+          <Title />
+          <Subtitle />
+          <Description />
+          <Stories />
+        </>
+      ),
+    },
+  },
+};
+
+export default preview;
