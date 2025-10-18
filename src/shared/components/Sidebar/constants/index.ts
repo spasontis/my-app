634b9c8435f9defbe09272f3e7c781946009926a
spasontis <smxxtdzmail@gmail.com>
2025-10-18 22:50:06 +0700

    feat: add sidebar component
    **Description**
     - Add sidebar component with prop
     - Tab: menu, task, workspace, account, settings

---
+++ src/shared/components/Sidebar/constants/index.ts
@@ -0,0 +1,12 @@
+import { ClipboardList, FileCode2, Menu, CircleUserRound, Cog } from 'lucide-react';
+
+export const TOP_ITEMS = [
+  { id: 'menu', Icon: Menu },
+  { id: 'task', Icon: ClipboardList },
+  { id: 'workspace', Icon: FileCode2 },
+];
+
+export const BOTTOM_ITEMS = [
+  { id: 'account', Icon: CircleUserRound },
+  { id: 'settings', Icon: Cog },
+];
---
+++ src/shared/components/Sidebar/stories/Sidebar.stories.tsx
@@ -0,0 +1,21 @@
+import type { Meta, StoryObj } from '@storybook/nextjs-vite';
+
+import { Sidebar } from '../ui/Sidebar';
+
+const meta = {
+  title: 'Example/Sidebar',
+  component: Sidebar,
+  tags: ['autodocs'],
+  argTypes: {
+    activeTab: { control: 'select', options: ['menu', 'task', 'workspace', 'account', 'settings'] },
+  },
+} satisfies Meta<typeof Sidebar>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    activeTab: 'task',
+  },
+};
