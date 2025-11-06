55ab5121ffa724fee9ef270385c690d3986e463f
spasontis <smxxtdzmail@gmail.com>
2025-11-06 13:10:48 +0700

    feat: add hint component
    **Description**
     - Added hint component with option
     - variant: 'default' | 'warning' | 'error' | 'success'

---
+++ src/shared/components/Hint/constants/index.tsx
@@ -0,0 +1,10 @@
+import { AlertCircle, CircleCheck, CircleX } from 'lucide-react';
+import { HintVariant } from '../types';
+
+export const DEFAULT_VARIANT: HintVariant = 'default';
+
+export const ICON = {
+  warning: AlertCircle,
+  success: CircleCheck,
+  error: CircleX,
+};
