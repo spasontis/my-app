b21249d48cf938ba7ea8bde2012bc3aafc3643b9
spasontis <smxxtdzmail@gmail.com>
2025-11-13 16:27:02 +0700

    feat: add app store

--- src/shared/constants/index.ts
+++ src/shared/constants/index.ts
@@ -1,1 +1,2 @@
-export const BRAND = 'codeway';
+export * from './company';
+export * from './statuses';
---
+++ src/shared/stores/app/types/index.ts
@@ -0,0 +1,30 @@
+import { UserRole } from '@/shared/constants';
+
+export type GuestStatus = 'guest';
+
+export interface TokenPayload {
+  login: string;
+  email: string;
+  role: UserRole;
+  exp: number;
+}
+
+export type Guest = {
+  isAuthenticated: false;
+  status: GuestStatus;
+};
+
+export type AuthenticatedUser = {
+  isAuthenticated: true;
+  id: number;
+  email: string;
+  role: UserRole;
+  accessToken: string;
+  tokenExpiry: number;
+};
+
+export type User = AuthenticatedUser | Guest;
+
+export interface AppState {
+  user: User;
+}
