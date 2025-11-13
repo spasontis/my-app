a128477fa5ce4de26971e575bf3a9fb29940ad57
spasontis <smxxtdzmail@gmail.com>
2025-11-14 06:07:59 +0700

    fix(app-store): fix store
    **Description**
     - Added token payload mapper
     - Fixed hook in actions and added actions
     - Installed jwt-decode

---
+++ src/shared/stores/app/actions/index.ts
@@ -0,0 +1,25 @@
+import { queryClient } from '@/shared/configs/queryClient';
+import { jwtDecode } from 'jwt-decode';
+import { INITIAL_STATE, ONE_SECOND } from '../constants';
+import { useAppStore } from '../hooks';
+import { TokenPayload } from '../types/index';
+import { mapTokenPayload } from '../mappers';
+
+export const setAuth = (accessToken: string) => {
+  const payload = jwtDecode<TokenPayload>(accessToken);
+  const mappedPayload = mapTokenPayload(payload);
+  const tokenExpiry = payload.exp ? payload.exp * ONE_SECOND : 0;
+
+  useAppStore.setState({
+    user: {
+      ...mappedPayload,
+      accessToken,
+      tokenExpiry,
+    },
+  });
+};
+
+export const clearAppStore = () => {
+  useAppStore.setState(INITIAL_STATE);
+  queryClient.clear();
+};
